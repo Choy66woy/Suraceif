ff2545abe15717d86ddb1f5db9bb21f004480a24
no <Choy66woy@gmail.com>
2017-12-06 17:18:35 -0800

    Updated game

--- js/states/running.js
+++ js/states/running.js
@@ -16,6 +16,7 @@ function running(game) {
         game.load.image('smalls', 'assets/smalls.png');
         game.load.image('jet', 'assets/jet.png');
         game.load.image('star', 'assets/star.png');
+        game.load.image('free', 'assets/free.png');
         game.load.spritesheet('Running', 'assets/Runstand.png', 50, 50);
         game.load.spritesheet('Shadow', 'assets/Shadow.png', 50, 50);
         game.load.spritesheet('robot', 'assets/robot.png', 50, 50);
@@ -25,6 +26,7 @@ function running(game) {
         game.load.spritesheet('sheet', 'assets/sheet.png', 100, 100);
         game.load.spritesheet('rowat', 'assets/rowat.png', 100, 100);
         game.load.spritesheet('clone', 'assets/clone.png', 100, 100);
+        game.load.spritesheet('pop', 'assets/pop.png', 100, 100);
 
 
 
@@ -58,9 +60,12 @@ function running(game) {
         wot.animations.play('wot');
 
         var wat = game.add.sprite(0, 200, 'clone');
-        wat.animations.add('wat', [0, 1, 2 , 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 98, 99, 100, 101, 102, 103, 104, 105, 106], 18, true);
+        wat.animations.add('wat', [0, 1, 2 , 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 98, 99, 100, 101, 102, 103, 104, 105, 106], 20, true);
         wat.animations.play('wat');
 
+        var t = game.add.sprite(1000, 200, 'pop');
+        t.animations.add('t', [0, 1, 2 , 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 98], 20, true);
+        t.animations.play('t');
 
 
 
@@ -85,6 +90,7 @@ function running(game) {
         platforms.create(600, 230, 'smalls');
         platforms.create(1300, 400, 'smalls');
         platforms.create(1000, 340, 'smalls');
+        platforms.create(800, 100, 'free');
 
 
 
